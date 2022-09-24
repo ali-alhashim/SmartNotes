2e6f31644848f7e9ea6a4c53419b95e80403378f
ALI MUSA ALHASHIM <93006102+ali-alhashim@users.noreply.github.com>
2022-09-24 13:09:31 +0300

    Signup

--- home/urls.py
+++ home/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('authorized', views.AuthorizedView.as_view()),
     path('login', views.LoginInterfaceView.as_view(), name='home.login'),
     path('logout', views.LogoutInterfaceView.as_view(), name='home.logout'),
+    path('signup', views.SignupView.as_view(), name='home.signup'),
 ]
  
--- home/views.py
+++ home/views.py
@@ -5,7 +5,15 @@ from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+
 # Create your views here.
+
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'home/register.html'
+    success_url = 'notes/list'
 
 class LoginInterfaceView(LoginView):
     template_name = 'home/login.html'
